4a87ec7a2640e068b57abc49959fe64b92831176
dylanjlennon <dylanjlennon@gmail.com>
2015-12-08 13:34:13 -0500

    major updates
    --still need to add dataframe list for each patient // diet prescription
    --still need to replace missing data

--- time_series_preprocessing.R
+++ time_series_preprocessing.R
@@ -1,8 +1,19 @@
+#---Probably need to make this run through patients in a dataframe list, and may need to make a data\
+#--frame list out of each patient for each diet prescription, and then run this code through that. 
+#---need to add in the fact that days that are 5 should all get NA values, but I need a clean sheet first.
+
+
+
 ######################################################
 #####Creating Time Series in R for Foodomics Data#####
 ######################################################
 
 #Required Packages (these will install if you don't have them installed)
+
+if(!require(plyr)){
+  install.packages("plyr")
+  library(plyr) #for formatting structure of data
+}
 if(!require(dplyr)){
   install.packages("dplyr")
   library(dplyr) #for formatting structure of data
@@ -46,10 +57,10 @@ foodomicsDB<-readRDS("foodomicsDB.RDS")
 ######################################################
 
 #Load the daily data
-daily_intakes<-read.csv(file='thsc_diet_data.csv', header=TRUE, sep=",")
+daily_intakes<-read.csv(file='015886550_intake.csv', header=TRUE, sep=",", na.strings=c("","NA"))
 
 #Take first two columns that have Date and ME number information
-daily_intakes<-daily_intakes[,c(1:2)]
+daily_intakes<-daily_intakes[,c(1:3)]
 
 #Rename these variables
 daily_intakes<-rename(daily_intakes, Date=Date.of.Intake)
@@ -59,15 +70,24 @@ dt <- data.table(daily_intakes) #after this the date is mm/dd/yy 12:00 AM
 test<-dt[, number := 1:.N, by = Date] #this numbers every row in the dt by date
 data<-as.data.frame(test) #this converts the dt to a dataframe
 
-data_wide <- reshape(data, direction="wide", idvar = "Date", timevar = "number")
+data_wide <- reshape(data, direction="wide", idvar = c("MRNUMBER","Date"), timevar = "number")
 #this reshape function casts the data frame into wide format, head(data_wide) to see
 #this creates a column for each ME, meaning the max number of ME a patient has 
 #is the number of columns that are created, different for each patient
 #Removes any rows that are empty
-data_wide<-data_wide[-which(data_wide == ""),]
+#data_wide<-data_wide[-which(data_wide == ""),]
+
+#This pastes all ME columns together
+cols<-names(data_wide[, c(3:ncol(data_wide))])
+data_wide$intakecode<- as.factor(ifelse(is.na(data_wide$ME.1), data_wide$ME.1,
+                                        apply( data_wide[ , cols ] , 1 , paste , collapse = "," )))
+
+########Maybe get rid of the other ME data
+data_wide<-data_wide[,c("MRNUMBER","Date","intakecode")]
+
 
 data_wide$Date<-as.POSIXct(data_wide$Date,format="%m/%d/%y") # now date is formatted yyyy/mm/dd
-data_wide.zoo<-zoo(data_wide[,-1],data_wide[,1]) #set date to Index in a zoo time series object
+data_wide.zoo<-zoo(data_wide[,],data_wide[,c("Date")]) #set date to Index in a zoo time series object
 df2 <- merge(data_wide.zoo,zoo(,seq(start(data_wide.zoo),end(data_wide.zoo),by="DSTday")), all=TRUE) 
 #fills in time series from the start of the first time series object to
 #the end of the first time series object
@@ -84,52 +104,54 @@ final$Date<-row.names(final)
 ###DATA TYPE###
 ###############
 #Get the data and aggregate it so that there is only one DATT value per diet record
-data_type<-read.csv(file='thsc_diet_data.csv', header=TRUE, sep=",")
-Data.Type<-data_type[,c(1,3)]
+data_type<-read.csv(file='015886550_intake.csv', header=TRUE, sep=",", na.strings=c("","NA"))
+Data.Type<-data_type[,c(1,2,4)]
 Data.Type<-rename(Data.Type, Date=Date.of.Intake)
 Data.Type<-rename(Data.Type, DATT=Data.Type)
-Data.Type<-aggregate(Data.Type$DATT, by=list(Data.Type$Date), max)
+Data.Type<-aggregate(Data.Type$DATT, by=list(Data.Type$Date,Data.Type$MRNUMBER), max)
 Data.Type<-rename(Data.Type, Date=Group.1)
+Data.Type<-rename(Data.Type, MRNUMBER=Group.2)
 Data.Type<-rename(Data.Type, DATT=x)
 
 #Fill in the time series
 Data.Type$Date<-as.POSIXct(Data.Type$Date,format="%m/%d/%y")
-Data.Type.zoo<-zoo(Data.Type[,-1],Data.Type[,1]) #set date to Index
+Data.Type.zoo<-zoo(Data.Type[,-1],Data.Type[,c("Date")]) #set date to Index
 df2 <- merge(Data.Type.zoo,zoo(,seq(start(Data.Type.zoo),end(Data.Type.zoo),by="DSTday")), all=TRUE)
 Data.Type<-as.data.frame(df2)
 Data.Type$Date<-row.names(Data.Type)
-Data.Type<-rename(Data.Type, DATT=df2)
+#Data.Type<-rename(Data.Type, DATT=df2)
 
 ###############
 ####DAY TYPE###
 ###############
 
 #Get the data and aggregate it so that there is only one value per date
-day_type<-read.csv(file='thsc_diet_data.csv', header=TRUE, sep=",")
-Day.Type<-day_type[,c(1,4)]
+day_type<-read.csv(file='015886550_intake.csv', header=TRUE, sep=",", na.strings=c("","NA"))
+Day.Type<-day_type[,c(1,2,5)]
 Day.Type<-rename(Day.Type, Date=Date.of.Intake)
 Day.Type<-rename(Day.Type, DAYT=Day.Type)
-Day.Type<-aggregate(Day.Type$DAYT, by=list(Day.Type$Date), max)
+Day.Type<-aggregate(Day.Type$DAYT, by=list(Day.Type$Date,Day.Type$MRNUMBER), max)
 Day.Type<-rename(Day.Type, Date=Group.1)
+Day.Type<-rename(Day.Type, MRNUMBER=Group.2)
 Day.Type<-rename(Day.Type, DAYT=x)
 
 #Fill in the time series
 Day.Type$Date<-as.POSIXct(Day.Type$Date,format="%m/%d/%y")
-Day.Type.zoo<-zoo(Day.Type[,-1],Day.Type[,1]) #set date to Index
+Day.Type.zoo<-zoo(Day.Type[,-1],Day.Type[,c("Date")]) #set date to Index
 df2 <- merge(Day.Type.zoo,zoo(,seq(start(Day.Type.zoo),end(Day.Type.zoo),by="DSTday")), all=TRUE)
 Day.Type<-as.data.frame(df2)
 Day.Type$Date<-row.names(Day.Type)
-Day.Type<-rename(Day.Type, DAYT=df2)
+#Day.Type<-rename(Day.Type, DAYT=df2)
 
 #MERGE DAY TYPE AND DATA TYPE
-types<-merge(Day.Type,Data.Type,by="Date")
+types<-merge(Day.Type,Data.Type,by=c("MRNUMBER","Date"))
 
 ###############
 ###MERGE ALL###
 ###############
 
 #MERGE THE DAILY INTAKE DATA WITH THE QUALITY DATA
-wide_daily_intakes<-merge(final,types,by="Date")
+wide_daily_intakes<-merge(final,types,by=c("MRNUMBER","Date"))
 #wide_daily_intakes<-wide_daily_intakes[,c(1:7,9:10)] #remove unneeded date column
 
 ######################################################
@@ -137,8 +159,8 @@ wide_daily_intakes<-merge(final,types,by="Date")
 ######################################################
 
 ####Now, we need to bring in the data about the diet prescription changes.
-diet_changes<-read.csv(file='thsc_ndt.csv', header=TRUE, sep=",")
-diet_changes<-diet_changes[,c(1,4,5,6,8,9,10,12,13,14,15)]
+diet_changes<-read.csv(file='thsc_ndt.csv', header=TRUE, sep=",") #NEED TO ADD MRNUMBER TO THIS FILE
+diet_changes<-diet_changes[,c(1,4,5,6,8,9,10,12,13,14,15)] ###WILL NEED TO CHANGE THESE COLUMN IDS
 diet_changes[is.na(diet_changes)]<- "0"
 diet_changes<-diet_changes[!diet_changes$Diet.Date=="", ]
 cols = c(2:11);    
@@ -161,7 +183,7 @@ diet_changes$sum2<-na.locf(diet_changes$sum)
 ###############
 
 #merge the diet change data with the daily intake data
-final_daily_intake<-merge(wide_daily_intakes,diet_changes,by="Date", all=TRUE)
+final_daily_intake<-merge(wide_daily_intakes,diet_changes[, c("Date", "sum2")],by="Date", all=TRUE)
 #Now we have a dataframe that we can use to implement LCOF and NOCB methods that rely on the changes in the Reason.For.Change column
 #We will need to prune this dataframe to the dates that the patient was actually on therapy for.
 
@@ -169,30 +191,70 @@ final_daily_intake<-merge(wide_daily_intakes,diet_changes,by="Date", all=TRUE)
 ##############FILL IN TIME SERIES DATA################
 ######################################################
 
-#Now, I am going to combine the columns such that we have one 'intake' column for meal equivalents and days
-#The code below will give the intake code a value of NA if the ME.1 is na, but it will give the intake code if there is an intake code.
-final_daily_intake$intakecode<- as.factor(ifelse(is.na(final_daily_intake$ME.1), final_daily_intake$ME.1,
-                                   paste(final_daily_intake$ME.1, 
-                                      final_daily_intake$ME.2,
-                                      final_daily_intake$ME.3,
-                                      final_daily_intake$ME.4,
-                                      final_daily_intake$ME.5,
-                                      final_daily_intake$ME.6,
-                                      final_daily_intake$DAYT,
-                                      final_daily_intake$DATT,sep=",")))
+####################
+#CALCULATE GAP DAYS#
+####################
 
+final_daily_intake$GAP<-is.na(final_daily_intake$intakecode)
+final_daily_intake$GAP<-as.character(final_daily_intake$GAP)
+#final_daily_intake<-final_daily_intake[, rowid:=1:.N, by = GAP]
+#final_daily_intake<-final_daily_intake[, number := 1:.N, by = GAP]
+final_daily_intake$GAP[final_daily_intake$GAP=="TRUE"] <- "0"  #0s indicate GAP days
+final_daily_intake$GAP[final_daily_intake$GAP=="FALSE"] <- "1"
+final_daily_intake$GAP<-as.numeric(final_daily_intake$GAP)
+final_daily_intake$No_Days <- unlist(sapply(rle(final_daily_intake$GAP)$lengths,                                      
+                                            function(x) 
+                                              if (x>1) 
+                                                seq_len(x)
+                                            else 0))
+
+final_daily_intake$GAP_Days<-ifelse(final_daily_intake$GAP==0,final_daily_intake$No_Days,"NA")
+
+
+final_daily_intake$GAP_LABEL<-""
+final_daily_intake <- within(final_daily_intake, {
+  GAP_LABEL <- !is.na(GAP_Days)
+  GAP_LABEL[!is.na(GAP_Days)] <- cumsum(GAP_Days[!is.na(GAP_Days)]==1)
+})
+
+final_daily_intake$GAP_LABEL_FIX<-ifelse(is.na(final_daily_intake$intakecode), final_daily_intake$GAP_LABEL,  NA)
+
+##GET THE GAP LABEL BACKWARDS SO THAT THE FIRST NON NA AND NEXT NON NA ARE IN SAME TIME SERIES
 setDT(final_daily_intake)
 #TIME SERIES INPUT
-      final_daily_intake[,intakecode:=na.locf(intakecode,na.rm=FALSE),
-                         by=list(as.factor(sum2))] #LOCF
-      final_daily_intake[,intakecode:=na.locf(intakecode,na.rm=FALSE,fromLast=TRUE),
+  final_daily_intake[,GAP_LABEL_FIX:=na.locf(GAP_LABEL_FIX,na.rm=TRUE,fromLast=TRUE)] #NOCB - I took out the last, by=MRNUMBER argument because it wasn't working
+
+#final_daily_intake$GAP_LABEL_FINAL<-ifelse(final_daily_intake$GAP==0,final_daily_intake$GAP_LABEL,"NA")
+
+#final_daily_intake2<-final_daily_intake[,c(1:10,13,16)]
+MaxVals<-aggregate(as.numeric(GAP_Days)~GAP_LABEL_FIX, data = final_daily_intake, max)
+#MaxVals<-rename(MaxVals, Max=GAP_Days) #doesn't work >?????
+final_daily_intake2<-merge(final_daily_intake,MaxVals, by="GAP_LABEL_FIX", drop=FALSE, all.x=TRUE)
+final_daily_intake2$Cutoff<-round(as.numeric(final_daily_intake2$`as.numeric(GAP_Days)`)/2)
+library(lubridate)
+library(plyr)
+final_daily_intake2$Date <- ymd(final_daily_intake2$Date)
+reallyfinal<-arrange(final_daily_intake2,Date)
+
+
+reallyfinal$cutoff2<-ifelse(!is.na(reallyfinal$intakecode) & reallyfinal$Cutoff >0, reallyfinal$Cutoff,  NA)
+
+reallyfinal<-as.data.frame(reallyfinal)
+reallyfinal2<- reallyfinal[,c(1:7,14)]
+reallyfinal2$cutoff2<-as.numeric(ifelse(is.na(reallyfinal2$cutoff2), '0', reallyfinal2$cutoff2))
+
+reallyfinal2$intakecode<- as.character(reallyfinal2$intakecode)
+  
+
+reallyfinal2<-as.data.frame(reallyfinal2)
+reallyfinal2 %>% group_by(z = cumsum(cutoff2 != 0)) %>%
+  mutate(intakecode = ifelse(unlist(lapply(cutoff2, function(x) rep(as.logical(x), max(1,x + 1))))[1:n()], intakecode[1], intakecode))
+
+
+setDT(reallyfinal2)
+reallyfinal2[,intakecode:=na.locf(intakecode,na.rm=FALSE,fromLast=TRUE),
                          by=list(as.factor(sum2))] #NOCB
 
-head(final_daily_intake)
-
+##TEXT TO COLUMNS FOR INTAKE CODE DATA, probably not ready to go yet
 setDT(final_daily_intake)[, paste0("Column", 1:8) := tstrsplit(intakecode, ",")]
-
-
 final_daily_intake<-str_split_fixed(final_daily_intake$intakecode, ",", 8)
-
-head(final_daily_intake)
