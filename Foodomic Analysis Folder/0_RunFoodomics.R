af119f892f33d7ba6f1f4d04b497f5e233e3aab8
dylanjlennon <dylanjlennon@gmail.com>
2016-04-29 15:55:50 -0400

    deleted trash
    removed trash

--- Foodomic Analysis Folder/0_RunFoodomics.R
+++ Foodomic Analysis Folder/0_RunFoodomics.R
@@ -1,14 +1,12 @@
 #To use this code:
 
-#First, set your working directory to ~/GitHub/foodomics/Foodomic Analysis Folder
-
-#Second, set variables
+#First, set variables
 
 patientfolder<-"G:/Data_D/D18/Clinic/Patient Folders/XXXXXXXXXXXXXX"
 kgid<-"KGXXXX.txt"
-  
 
-#Third, run a foodomics analysis
+#Second, run a foodomics analysis
+
 #Source the files needed in the analysis
 setwd("~/GitHub/foodomics/Foodomic Analysis Folder")
 source("1_TS_PreProcessing.R")
