785a87cfda60073cfa87309610d0ab6dda8ba860
Dylan Lennon <dylanjlennon@gmail.com>
2016-05-26 15:40:52 -0400

    initial commit
    ##can read NFD from MySQL
    ##can update values in MySQL database
    ##can persistently store changes
    ##can delete records
    ##can tag foods as "pending" which will remove them from the meal making
    database
    ##created a meal making database table interface

---
+++ CRUD_app/server.R
@@ -0,0 +1,185 @@
+if(!require(shiny)){
+  install.packages("shiny")
+  library(shiny) 
+}
+
+if(!require(RMySQL)){
+  install.packages("RMySQL")
+  library(RMySQL) 
+}
+
+if(!require(DT)){
+  install.packages("DT")
+  library(DT) 
+}
+
+if(!require(shinydashboard)){
+  install.packages("shinydashboard")
+  library(shinydashboard) 
+}
+
+
+
+
+fields <- c(Year="Year",
+            NDID="NDID",
+            Calories_per_serving_kcal="Calories_per_serving_kcal",
+            Saturated_Fat_per_serving_g="Saturated_Fat_per_serving_g",
+            Trans_Fat_per_serving_g="Trans_Fat_per_serving_g",
+            Monounsaturated_Fat_per_serving_g="Monounsaturated_Fat_per_serving_g",
+            Polyunsaturated_Fat_per_serving_g="Polyunsaturated_Fat_per_serving_g",
+            Pend_Food = "Pend_Food")
+
+
+library(RMySQL)
+options(mysql = list(
+  user="dlennon",
+  password="cNrYqWax1",
+  host="if-srvv-borum",
+  dbname="borum_practice"
+)) 
+
+TABLE_NAME <- "foodomics_database_test"
+
+save_data_mysql <- function(formdata) {
+  query <- sprintf("INSERT INTO %s (%s) VALUES ('%s')", 
+                   TABLE_NAME, paste(names(formdata), collapse = ", "), paste(formdata, collapse = "', '"))
+  mysql_query(query)
+}
+
+delete_data_mysql <- function(selected, formdata) {
+  key <- strsplit(selected," ")[[2]]
+  query <- sprintf("DELETE FROM %s WHERE NFD_Key='%s'", 
+                   TABLE_NAME, key)
+  mysql_query(query)
+}
+
+edit_data_mysql <- function(selected, formdata) {
+  key <- selected[[2]]
+  query <- sprintf("UPDATE %s SET %s WHERE NFD_Key='%s'", 
+                   TABLE_NAME, paste(names(formdata),"='",formdata,"'",sep = "",collapse = ", "), key)
+  mysql_query(query)
+}
+
+load_data_mysql <- function() {
+  query <- sprintf("SELECT * FROM %s WHERE Year='2001'", TABLE_NAME)
+  mysql_query(query)
+}
+
+load_data_makemeals <- function() {
+  query <- sprintf("SELECT * FROM %s WHERE Year='2001'", TABLE_NAME)
+  mysql_query(query)
+}
+
+mysql_query <- function(query) {
+  print(query)
+  db <- dbConnect(MySQL(), dbname = options()$mysql$dbname, host = options()$mysql$host, 
+                  user = options()$mysql$user, 
+                  password = options()$mysql$password)
+  data <- dbGetQuery(db, query)
+  dbDisconnect(db)
+  data
+}
+
+
+server <- shinyServer(
+  function(input, output, session) {
+
+
+    # Whenever a field is filled, aggregate all form data
+    formData <- reactive({
+      data <- sapply(
+        fields, 
+        function(x){
+        input[[x]]
+        }
+      )
+      data
+    })
+    
+    
+    # When buttons are clicked, run the appropriate function with the form data
+    observeEvent(input$submit, { save_data_mysql(formData()) })
+    observeEvent(input$delete, { delete_data_mysql(input$rows, formData()) })
+    observeEvent(input$edit, { edit_data_mysql(input$rows, formData()) })
+    observeEvent(input$rows, {
+      updateTextInput(session,"Year",value=input$rows[[3]])
+      updateTextInput(session,"NDID",value=input$rows[[4]])
+      updateNumericInput(session,"Calories_per_serving_kcal",value=input$rows[[5]])
+      updateNumericInput(session,"Saturated_Fat_per_serving_g",value=input$rows[[6]])
+      updateNumericInput(session,"Trans_Fat_per_serving_g",value=input$rows[[7]])
+      updateNumericInput(session,"Monounsaturated_Fat_per_serving_g",value=input$rows[[8]])
+      updateNumericInput(session,"Polyunsaturated_Fat_per_serving_g",value=input$rows[[9]])
+      updateTextInput(session,"Pend_Food",value=input$rows[[10]])
+      
+      
+    })
+    
+    
+    # Show the previous test
+    # (update with current response when Submit is clicked)
+    output$test = renderDataTable({
+      datatable(
+        {
+          input$submit
+          input$delete
+          input$edit
+          load_data_mysql()
+        },
+        selection = 'none',
+        callback = JS(
+          "table.on('click.dt', 'tr', function() {
+          $('tr').removeClass('selected');
+          $(this).addClass('selected');
+          Shiny.onInputChange('rows', table.rows('.selected').data().toArray());
+    });"),
+        options = list(scrollX = TRUE)
+        
+        )
+  })
+    
+    
+    
+    
+
+    
+    # Show the previous test
+    # (update with current response when Submit is clicked)
+    output$makemeals = renderDataTable({
+      datatable(
+        {
+          load_data_makemeals()
+        },options = list(scrollX = TRUE, columnDefs = list(list(width = '200px', targets = "c(1)")),searchHighlight = TRUE,pageLength = 10,initComplete = JS(
+          "function(settings, json) {",
+          "$(this.api().table().header()).css({'background-color': 'black', 'color': 'white'});",
+          "}")), class="stripe compact nowrap", rownames = FALSE
+        
+      ) %>%
+        formatStyle(
+          'Saturated_Fat_per_serving_g',
+          color = styleInterval(c(25, 75), c('white', 'black', 'white')),
+          backgroundColor = styleInterval(c(25, 75), c('blue', 'white', 'red'))
+        ) %>%
+        formatStyle(
+          'Monounsaturated_Fat_per_serving_g',
+          color = styleInterval(c(25, 75), c('white', 'black', 'white')),
+          backgroundColor = styleInterval(c(25, 75), c('blue', 'white', 'red'))
+        ) %>%
+        formatStyle(
+          'Polyunsaturated_Fat_per_serving_g',
+          color = styleInterval(c(25, 75), c('white', 'black', 'white')),
+          backgroundColor = styleInterval(c(25, 75), c('blue', 'white', 'red'))
+        )
+        
+        
+        
+    }) 
+    
+    
+    
+    
+    
+  }
+  
+
+      )
---
+++ CRUD_app/ui.R
@@ -0,0 +1,88 @@
+if(!require(shiny)){
+  install.packages("shiny")
+  library(shiny) #for formatting structure of data
+}
+
+if(!require(RMySQL)){
+  install.packages("RMySQL")
+  library(RMySQL) #for formatting structure of data
+}
+
+if(!require(DT)){
+  install.packages("DT")
+  library(DT) #for formatting structure of data
+}
+
+if(!require(shinydashboard)){
+  install.packages("shinydashboard")
+  library(shinydashboard) #for formatting structure of data
+}
+
+#Create header for dashboard page
+header<-dashboardHeader(title = HTML(paste(icon('globe'),'Foodomics Database')))
+
+#Create body for dashboard page
+body<-dashboardBody(
+  
+  tabItems(
+    
+    # First tab content
+    tabItem(tabName = "Data",
+            #***app1***
+            
+            fluidRow(
+        column(width = 9,
+           DT::dataTableOutput("test")
+           
+           ), tags$hr(),
+        
+        column(
+            width=3,
+          numericInput("Year","Year",""),
+          textInput("NDID","NDID",""),
+          numericInput("Calories_per_serving_kcal","Calories_per_serving_kcal",""),
+          numericInput("Saturated_Fat_per_serving_g","Saturated_Fat_per_serving_g",""),
+          numericInput("Trans_Fat_per_serving_g","Trans_Fat_per_serving_g",""),
+          numericInput("Monounsaturated_Fat_per_serving_g","Monounsaturated_Fat_per_serving_g",""),
+          numericInput("Polyunsaturated_Fat_per_serving_g","Polyunsaturated_Fat_per_serving_g",""),
+          checkboxInput("Pend_Food","Pend this Food",value=TRUE, width=NULL),
+        
+          actionButton("submit", "Submit"),
+          actionButton("edit", "Edit"),
+          actionButton("delete", "Delete")
+      
+          )
+      )
+    ),
+   
+    
+    # Second tab content
+    tabItem(tabName = "Create",
+           
+            fluidRow(
+              #***Meal Making Table***
+              column(width = 9,
+                     DT::dataTableOutput("makemeals")),
+              #***Help Text***
+              column(width = 3,
+                     helpText("Make PKT meals using these data."))
+             )
+    )
+  )
+)
+  
+  
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Data", tabName = "Data", icon = icon("dashboard")),
+    menuItem("Create", tabName = "Create", icon = icon("dashboard"))
+  )
+  )
+  
+  
+  dashboardPage(skin = "blue",
+                header,
+                sidebar,
+                body
+  )
